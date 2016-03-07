77588045b9e4a7c97178b523a535c765f92c7f6e
ruixingw <ruixingw@gmail.com>
2016-03-07 19:19:58 +0800

    Added many class

--- geomdef.py
+++ geomdef.py
@@ -1,33 +1,53 @@
 from __future__ import print_function
 import math
 
-def addatom(elementid,xyz):
-    at.append(atoms(elementid,xyz))
-    return len(at)-1
-def addbond(atomid1,atomid2):
-    bonds(atomid1,atomid2)
-def addangle(atomid1,atomid2,atomid3):
-    angles(atomid1,atomid2,atomid3)
-def adddihd(atomid1,atomid2,atomid3,atomid4):
-    dihedrals(atomid1,atomid2,atomid3,atomid4)
 def swap(a,b):
     return b,a
 
-class atoms(object):
-    number=0
-    bohr=0.5291772086 # bohr to angstrom
-    idtoname={'1':'H','5':'B','6':'C','7':'N','8':'O','9':'F','13':'Al','14':'Si','15':'P','16':'S','17':'Cl','26':'Fe','28':'Ni','29':'Cu','30':'Zn'}
+class geometry(object):
 
-    def __init__(self,elementid,xyz):  # int,[float,float,float]
+    def __init__(self):
+        self.natoms=0
+        self.atoms=[0]
+        self.atomsnumber=0
+        self.bonds={}
+        self.angles={}
+        self.dihedrals={}
+        self.bondfunc={}
+        self.anglefunc={}
+        self.dihdfunc={}
+    def addatom(self,elementid,xyz):
+        atoms(self,elementid,xyz)
+        self.natoms+=1
+    def addbond(self,atomid1,atomid2):
+        bonds(self,atomid1,atomid2)
+    def addangle(self,atomid1,atomid2,atomid3):
+        angles(self,atomid1,atomid2,atomid3)
+    def adddihd(self,atomid1,atomid2,atomid3,atomid4):
+        dihedrals(self,atomid1,atomid2,atomid3,atomid4)
+    def addbondfunc(self,value):
+        bondfunc(self,value)
+    def addanglefunc(self,value):
+        anglefunc(self,value)
+    def adddihdfunc(self,value):
+        dihdfunc(self,value)
+
+class atoms(object):
+
+    bohr=0.5291772086 # bohr to angstrom
+    idtoname={1:'H',5:'B',6:'C',7:'N',8:'O',9:'F',13:'Al',14:'Si',15:'P',16:'S',17:'Cl',26:'Fe',28:'Ni',29:'Cu',30:'Zn'}
+
+    def __init__(self,geom,elementid,xyz):  # int,[float,float,float]
         self.elementid=elementid   # int
-        self.elementname=atoms.idtoname[str(elementid)] #str
+        self.elementname=atoms.idtoname[elementid] #str
         self.atomtype=''
         self.charge=''  # char
         self.x=xyz[0]*atoms.bohr
         self.y=xyz[1]*atoms.bohr
         self.z=xyz[2]*atoms.bohr
-        atoms.number+=1
-        self.atomid=atoms.number
+        geom.atomsnumber+=1
+        self.atomid=geom.atomsnumber
+        geom.atoms.append(self)
     def xyz(self):
         return [self.x,self.y,self.z]
 
@@ -40,37 +60,25 @@ class vector(object):
         return (self.x**2+self.y**2+self.z**2)**(0.5)
 
 class bonds(object):
-    list={}
-    number=0
-    def __init__(self,a,b): # self, atomid a, atomid b
+    def __init__(self,geom,a,b): # self, atomid a, atomid b
         if a>b:
             a,b=swap(a,b)
-        self.a=at[a]
-        self.b=at[b]
-        self.bf=''
-        bonds.number+=1
-        self.number=bonds.number
-        bonds.list.update({str(a)+','+str(b):self})
-        bonds.list.update({str(b)+','+str(a):self})
+        self.a=geom.atoms[a]
+        self.b=geom.atoms[b]
+        geom.bonds.update({str(a)+','+str(b):self})
         self.vec=vector(self.a.x-self.b.x,self.a.y-self.b.y,self.a.z-self.b.z)
     def length(self):
         return self.vec.length()
 class angles(object):
-    list={}
-    number=0
-    def __init__(self,a,b,c):
+    def __init__(self,geom,a,b,c):
         if a>c:
             a,c=swap(a,c)
-        self.a=at[a]
-        self.b=at[b]
-        self.c=at[c]
-        self.af=''
+        self.a=geom.atoms[a]
+        self.b=geom.atoms[b]
+        self.c=geom.atoms[c]
         self.ab=vector(self.a.x-self.b.x,self.a.y-self.b.y,self.a.z-self.b.z)
         self.bc=vector(self.b.x-self.c.x,self.b.y-self.c.y,self.b.z-self.c.z)
-        angles.number+=1
-        self.number=angles.number
-        angles.list.update({str(a)+','+str(b)+','+str(c):self})
-        angles.list.update({str(c)+','+str(b)+','+str(a):self})
+        geom.angles.update({str(a)+','+str(b)+','+str(c):self})
 
     def angle(self):
         innerproduct=self.ab.x*self.bc.x+self.ab.y*self.bc.y+self.ab.z*self.bc.z
@@ -79,37 +87,34 @@ class angles(object):
         angle=180-math.acos(cos)*180/math.pi
         return angle
 class dihedrals(object):
-    list={}
-    number=0
-    def __init__(self,a,b,c,d):
+    def __init__(self,geom,a,b,c,d):
         if b>c:
             b,c=swap(b,c)
             a,d=swap(a,d)
         elif b==c:
             if a>d:
                 a,d=swap(a,d)
-        self.a=at[a]
-        self.b=at[b]
-        self.c=at[c]
-        self.d=at[d]
-        self.df=''
-        dihedrals.number+=1
-        dihedrals.list.update({str(a)+','+str(b)+','+str(c)+','+str(d):self})
-        dihedrals.list.update({str(d)+','+str(c)+','+str(b)+','+str(a):self})
+        self.a=geom.atoms[a]
+        self.b=geom.atoms[b]
+        self.c=geom.atoms[c]
+        self.d=geom.atoms[d]
+
+        geom.dihedrals.update({str(a)+','+str(b)+','+str(c)+','+str(d):self})
+
 
 class bondfunc(object):
-    list={}
-    def __init__(self,bondobj):
+
+    def __init__(self,geom,bondobj):
         a=bondobj.a.atomtype
         b=bondobj.b.atomtype
         if a>b:
             a,b=swap(a,b)
         bondobj.func=self
         self.link=a+' '+b
-        bondfunc.list.update({self.link:self})
+        geom.bondfunc.update({self.link:self})
 class anglefunc(object):
-    list={}
-    def __init__(self,angleobj):
+
+    def __init__(self,geom,angleobj):
         a=angleobj.a.atomtype
         b=angleobj.b.atomtype
         c=angleobj.c.atomtype
@@ -117,10 +122,10 @@ class anglefunc(object):
             a,c=swap(a,c)
         angleobj.func=self
         self.link=a+' '+b+' '+c
-        anglefunc.list.update({self.link:self})
+        geom.anglefunc.update({self.link:self})
 class dihdfunc(object):
-    list={}
-    def __init__(self,dihdobj):
+
+    def __init__(self,geom,dihdobj):
         a=dihdobj.a.atomtype
         b=dihdobj.b.atomtype
         c=dihdobj.c.atomtype
@@ -133,10 +138,4 @@ class dihdfunc(object):
                 a,d=swap(a,d)
         dihdobj.func=self
         self.link=a+' '+b+' '+c+' '+d
-        dihdfunc.list.update({self.link:self})
-
-
-
-
-at=['0']
-xyz=['']
+        geom.dihdfunc.update({self.link:self})
--- readfiles.py
+++ readfiles.py
@@ -1,5 +1,5 @@
 # From fchk read Charge, Multiplicity, Coordinates
-
+import os,time
 class readfchk(object):
 
     def __init__(self,file):
@@ -32,6 +32,12 @@ class readfchk(object):
                     break
         self.xyzlist=[float(x) for x in self.xyzlist]
         self.atomlist=[int(x) for x in self.atomlist]
+    def assignatom(self,geom):
+        xyz=['']
+        for i in range(0,len(self.xyzlist),3):
+            xyz.append(self.xyzlist[i:i+3])
+        for i in range(1,self.natoms+1):
+            geom.addatom(self.atomlist[i],xyz[i])
 class readac(object):
     def __init__(self,file):
         self.atomtype=['']
@@ -45,6 +51,12 @@ class readac(object):
                 ac=string.split()
                 self.atomtype.append(ac[len(ac)-1])
                 self.charge.append(ac[len(ac)-2])
+    def assigntype(self,geom):
+        for i in range(1,geom.natoms+1):
+            geom.atoms[i].atomtype=self.atomtype[i]
+    def assigncharge(self,geom):
+        for i in range(1,geom.natoms+1):
+            geom.atoms[i].charge=self.charge[i]
 
 class readlog(object):
     def __init__(self,file):
@@ -77,3 +89,53 @@ class readlog(object):
                 print("No Freq found")
                 return ['No Freq found']
         return freq
+
+
+class gauFile(object):
+    g09rt='g09boon'
+    g09a2rt='g09a2boon'
+    antechamber='antechamber -c resp'
+    def __init__(self,name):
+        self.name=name
+        self.com=name+'.com'
+        self.log=name+'.log'
+        self.chk=name+'.chk'
+        self.fchk=name+'.fchk'
+        self.ac=name+'.ac'
+    def rung09(self):
+        ifchk=1
+        with open(self.com,'r') as f:
+            for line in f.readlines():
+                if line.find('%chk')>=0:
+                    ifchk=0
+            if ifchk==1:
+                f.seek(0)
+                content=f.read()
+        if ifchk==1:
+            with open(self.com,'w') as f:
+                f.write('%chk='+self.chk+'\n')
+                f.write(content)
+        print('Run g09 : '+gauFile.g09rt+' '+self.com)
+        os.system(gauFile.g09rt+' '+self.com)
+    def rung09a2(self):
+        print('Run g09a2 : '+gauFile.g09a2rt+' '+self.com)
+        os.system(gauFile.g09a2rt+' '+self.com)
+    def isover(self):
+        print('Waiting g09 for',self.com,'...')
+        while True:
+            output=''
+            if not os.path.isfile(self.log):
+                continue
+            with open(self.log,'r') as f:
+                for x in f.readlines()[:-6:-1]:
+                    output+=x
+            if output.find('Normal termination')>=0:
+                print('    ..normal termination')
+                return True
+            if output.find('Error termination')>=0:
+                print('Error termination in '+self.com)
+                return False
+            time.sleep(2)
+    def getresp(self):
+        print('Runing antechamber: \n')
+        os.system(gauFile.antechamber+' -i '+self.log+' -fi gout -o '+self.ac+' -fo ac')
