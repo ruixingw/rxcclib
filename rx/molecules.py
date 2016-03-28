6e4f8d18dd73512ead656a1f6135ded9713fd01c
ruixingw <ruixingw@gmail.com>
2016-03-28 15:07:51 +0800

    Finished molecules.py

--- rx/molecules.py
+++ rx/molecules.py
@@ -1,6 +1,5 @@
 # Molecules, atoms definition
 from __future__ import print_function
-import math
 import numpy as np
 
 class MoleDefError(Exception):
@@ -8,7 +7,6 @@ class MoleDefError(Exception):
         self.value=value
     def __str__(self):
         return repr(self.value)
-
 
 class molecule(object):
     def __init__(self,moleculename):
@@ -20,32 +18,50 @@ class molecule(object):
         self.__bondlist={}
         self.__anglelist={}
         self.__dihdlist={}
-        self.bondfunc={}
-        self.anglefunc={}
-        self.dihdfunc={}
+        self.__bondfunclist={}
+        self.__anglefunclist={}
+        self.__dihdfunclist={}
         self.__pointer=0
     @property
     def natoms(self):
         return len(self.__atomlist)-1
+    # Add atom and internal coordinates
     def addatom(self,idorsym,xyz,unit='bohr'):
         self.__atomlist.append(Atom(self,idorsym,xyz,unit))
+
     def addbond(self,atomnum1,atomnum2):
+        if atomnum1>atomnum2:
+            atomnum1,atomnum2=atomnum2,atomnum1
         self.__bondlist.update({str(atomnum1)+','+str(atomnum2):Bond(self,atomnum1,atomnum2)})
+
     def addangle(self,atomnum1,atomnum2,atomnum3):
+        if atomnum1>atomnum3:
+            atomnum1,atomnum3=atomnum3,atomnum1
         self.__anglelist.update({str(atomnum1)+','+str(atomnum2)+','+str(atomnum3):Angle(self,atomnum1,atomnum2,atomnum3)})
+
     def adddihd(self,atomnum1,atomnum2,atomnum3,atomnum4):
+        if atomnum2>atomnum3:
+            atomnum2,atomnum3=atomnum3,atomnum2
+            atomnum1,atomnum4=atomnum4,atomnum1
+        elif atomnum2==atomnum3:
+            if atomnum1>atomnum4:
+                atomnum1,atomnum4=atomnum4,atomnum1
         self.__dihdlist.update({str(atomnum1)+','+str(atomnum2)+','+str(atomnum3)+','+str(atomnum4):Dihd(self,atomnum1,atomnum2,atomnum3,atomnum4)})
 
+    # Get atom and internal coordiantes
     def atom(self,atomnum):
         return self.__atomlist[atomnum]
+
     def bond(self,atomnum1,atomnum2):
         if atomnum1>atomnum2:
             atomnum1,atomnum2=atomnum2,atomnum1
         return self.__bondlist[str(atomnum1)+','+str(atomnum2)]
+
     def angle(self,atomnum1,atomnum2,atomnum3):
         if atomnum1>atomnum3:
             atomnum1,atomnum3=atomnum3,atomnum1
         return self.__anglelist[str(atomnum1)+','+str(atomnum2)+','+str(atomnum3)]
+
     def dihd(self,atomnum1,atomnum2,atomnum3,atomnum4):
         if atomnum2>atomnum3:
             atomnum2,atomnum3=atomnum3,atomnum2
@@ -55,13 +71,61 @@ class molecule(object):
                 atomnum1,atomnum4=atomnum4,atomnum1
         return self.__dihdlist[str(atomnum1)+','+str(atomnum2)+','+str(atomnum3)+','+str(atomnum4)]
 
+    # Add MM functions
+    def addbondfunc(self,bondobj):
+        atype=bondobj[1].atomtype
+        btype=bondobj[2].atomtype
+        if atype>btype:
+            atype,btype=btype,atype
+        link=atype+' '+btype
+        iffind=False
+        for key,value in self.__bondfunclist.items():
+            if key.link==link:
+                value.append(bondobj)
+                iffind=True
+        if iffind==False:
+            self.__bondfunclist.update({Bondfunc(self,bondobj):[bondobj]})
+        bondobj.bondtype=link
 
-    def addbondfunc(self,value):
-        bondfunc(self,value)
-    def addanglefunc(self,value):
-        anglefunc(self,value)
-    def adddihdfunc(self,value):
-        dihdfunc(self,value)
+    def addanglefunc(self,angleobj):
+        atype=angleobj[1].atomtype
+        btype=angleobj[2].atomtype
+        ctype=angleobj[3].atomtype
+        if atype>ctype:
+            atype,ctype=ctype,atype
+        link=atype+' '+btype+' '+ctype
+        iffind=False
+        for key,value in self.__anglefunclist.items():
+            if key.link==link:
+                value.append(angleobj)
+                iffind=True
+        if iffind==False:
+            self.__anglefunclist.update({Anglefunc(self,angleobj):[angleobj]})
+        angleobj.angletype=link
+
+    def adddihdfunc(self,dihdobj):
+        atype=dihdobj[1].atomtype
+        btype=dihdobj[2].atomtype
+        ctype=dihdobj[3].atomtype
+        dtype=dihdobj[4].atomtype
+        if btype>ctype:
+            atype,dtype=dtype,atype
+            btype,ctype=ctype,btype
+        elif btype==ctype:
+            if atype>dtype:
+                atype,dtype=dtype,atype
+        link=atype+' '+btype+' '+ctype+' '+dtype
+        iffind=False
+        for key,value in self.__dihdfunclist.items():
+            if key.link==link:
+                value.append(dihdobj)
+                iffind=True
+        if iffind==False:
+            self.__dihdfunclist.update({Dihdfunc(self,dihdobj):[dihdobj]})
+        dihdobj.dihdtype=link
+
+
+    # getitem and iteration
     def __getitem__(self,key):
         if isinstance(key,int):
             return self.__atomlist[key]
@@ -84,6 +148,15 @@ class molecule(object):
         else:
             raise StopIteration("Exceeded all atoms")
 
+
+    # Read structure from xyz
+    def readfromxyz(self,filename):
+        with open(filename) as f:
+            for line in f.readlines():
+                this=line.split()
+                atom=this[0]
+                xyz=np.array([this[1],this[2],this[3]],dtype=float)
+                self.addatom(atom,xyz,unit='angstrom')
 
 class Atom(object):
     '''
@@ -120,25 +193,24 @@ class Atom(object):
             try:
                 self.__atomsym=Atom.__idtosym[self.__elementid] #str
             except KeyError:
-                print("Error when adding atom: Idtosym not defined for atomic no:",self.__elementid)
-                quit()
+                raise MoleDefError("Error when adding atom: Idtosym not defined for atomic no:",self.__elementid)
         elif isinstance(idorsym,str):
             self.__atomsym=idorsym
             try:
                 self.__elementid=Atom.__symtoid[self.__atomsym]
             except KeyError:
-                print("Error when adding atom: Idtosym not defined for atomic symbol:",self.__atomsym)
-                quit()
+                raise MoleDefError("Error when adding atom: Idtosym not defined for atomic symbol:",self.__atomsym)
         else:
-                print("Error when adding atom: Expected atomic NO(int) or symbol(str) for input, received a",type(idorsym))
-                quit()
+                raise MoleDefError("Error when adding atom: Expected atomic NO(int) or symbol(str) for input, received a",type(idorsym))
+
         if unit=='bohr':
-            self.__xyz=xyz*Atom.bohr
+            self.__xyz=Atom.bohr*xyz
         elif unit=='angstrom':
             self.__xyz=xyz
 
         self.__atomnum=mole.natoms+1
-        return
+        self.__atomtype=self.name
+
     @property
     def xyz(self):
         return self.__xyz
@@ -154,6 +226,12 @@ class Atom(object):
     @property
     def atomsym(self):
         return self.__atomsym
+    @property
+    def atomtype(self):
+        return self.__atomtype
+    @atomtype.setter
+    def atomtype(self,value):
+        self.__atomtype=value
     @property
     def mymolecule(self):
         return self.__mymolecule
@@ -172,7 +250,20 @@ class Bond(object):
         self.__a=mole[a]
         self.__b=mole[b]
         self.__vec=self.__a.xyz-self.__b.xyz
-
+        self.bondtype=self.__a.name+' '+self.__b.name
+    def __getitem__(self,value):
+        if value==1:
+            return self.__a
+        elif value==2:
+            return self.__b
+        else:
+            raise MoleDefError("Argument for __getitem__ of bond object must be 1 or 2.")
+    @property
+    def a(self):
+        return self.__a
+    @property
+    def b(self):
+        return self.__b
     @property
     def length(self):
         return np.linalg.norm(self.__vec)
@@ -185,6 +276,26 @@ class Angle(object):
         self.__c=mole[c]
         self.__ab=mole[a].xyz-mole[b].xyz
         self.__bc=mole[b].xyz-mole[c].xyz
+        self.angletype=self.__a.name+' '+self.__b.name+' '+self.__c.name
+    def __getitem__(self,value):
+        if value==1:
+            return self.__a
+        elif value==2:
+            return self.__b
+        elif value==3:
+            return self.__c
+        else:
+            raise MoleDefError("Argument for __getitem__ of angle object must be 1, 2 or 3.")
+
+    @property
+    def a(self):
+        return self.__a
+    @property
+    def b(self):
+        return self.__b
+    @property
+    def c(self):
+        return self.__c
     @property
     def anglevalue(self):
         v1u=self.__ab/np.linalg.norm(self.__ab)
@@ -208,6 +319,32 @@ class Dihd(object):
         self.__b=mole[b]
         self.__c=mole[c]
         self.__d=mole[d]
+        self.dihdtype=self.__a.name+' '+self.__b.name+' '+self.__c.name+' '+self.__d.name
+    def __getitem__(self,value):
+        if value==1:
+            return self.__a
+        elif value==2:
+            return self.__b
+        elif value==3:
+            return self.__c
+        elif value==4:
+            return self.__d
+        else:
+            raise MoleDefError("Argument for __getitem__ of angle object must be 1, 2 ,3 or 4.")
+
+    @property
+    def a(self):
+        return self.__a
+    @property
+    def b(self):
+        return self.__b
+    @property
+    def c(self):
+        return self.__c
+    @property
+    def d(self):
+        return self.__d
+
     @property
     def dihdvalue(self):
         v1=self.__a.xyz-self.__b.xyz
@@ -226,45 +363,39 @@ class Dihd(object):
                 return 180.0
         return dihd
 
-class bondfunc(object):
-
-    def __init__(self,molecule,bondobj):
-        a=bondobj.a.atomtype
-        b=bondobj.b.atomtype
+class Bondfunc(object):
+    def __init__(self,mole,bondobj):
+        a=bondobj[1].atomtype
+        b=bondobj[2].atomtype
         if a>b:
             a,b=b,a
-        bondobj.func=self
         self.link=a+' '+b
-        molecule.bondfunc.update({self.link:self})
-class anglefunc(object):
+
+
+class Anglefunc(object):
 
     def __init__(self,molecule,angleobj):
-        a=angleobj.a.atomtype
-        b=angleobj.b.atomtype
-        c=angleobj.c.atomtype
+        a=angleobj[1].atomtype
+        b=angleobj[2].atomtype
+        c=angleobj[3].atomtype
         if a>c:
             a,c=c,a
-        angleobj.func=self
         self.link=a+' '+b+' '+c
-        molecule.anglefunc.update({self.link:self})
-class dihdfunc(object):
+
+class Dihdfunc(object):
 
     def __init__(self,molecule,dihdobj):
-        a=dihdobj.a.atomtype
-        b=dihdobj.b.atomtype
-        c=dihdobj.c.atomtype
-        d=dihdobj.d.atomtype
+        a=dihdobj[1].atomtype
+        b=dihdobj[2].atomtype
+        c=dihdobj[3].atomtype
+        d=dihdobj[4].atomtype
         if b>c:
             a,d=d,a
             b,c=c,b
         elif b==c:
             if a>d:
                 a,d=d,a
-        dihdobj.func=self
         self.link=a+' '+b+' '+c+' '+d
-        molecule.dihdfunc.update({self.link:self})
-
-
 
 if __name__=='__main__':
     import doctest
--- rx/testmole.py
+++ rx/testmole.py
@@ -6,14 +6,28 @@ class TestMole(unittest.TestCase):
     def setUp(self):
         self.mole=rxmol.molecule("H2O2")
         self.assertTrue(isinstance(self.mole,rxmol.molecule))
+
         self.mole.addatom('O',np.array([0.0,0.0,0.0]),unit='angstrom')
         self.mole.addatom('H',np.array([0.0,1.0,0.0]),unit='angstrom')
         self.mole.addatom('O',np.array([0.0,0.0,1.0]))
         self.mole.addatom('H',np.array([1.0,0.0,0.0]),unit='angstrom')
         self.assertTrue(isinstance(self.mole[1],rxmol.Atom))
+
+        self.mole.addbond(1,2)
+        self.mole.addbond(1,3)
+        self.mole.addbond(3,4)
+
+        self.mole.addangle(2,1,3)
+        self.mole.addangle(4,3,1)
+        self.assertTrue(isinstance(self.mole.angle(2,1,3),rxmol.Angle))
+
+        self.mole.adddihd(2,1,3,4)
+        self.assertTrue(isinstance(self.mole.dihd(4,3,1,2),rxmol.Dihd))
+
+
         # @property natoms
         self.assertEqual(self.mole.natoms,4)
-        # @property atomlist
+
         self.assertEqual(self.mole.atom(1).elementid,8)
         # def __iter__
         ite=iter(self.mole)
@@ -25,7 +39,7 @@ class TestMole(unittest.TestCase):
         # def __getitem__
         self.assertEqual(str(self.mole[1].name),'O1')
 
-    def test_atom(self):
+    def test_mole(self):
         # @property xyz
         np.testing.assert_array_equal(self.mole[1].xyz,np.array([0.0,0.0,0.0]))
         np.testing.assert_array_equal(self.mole[2].xyz,np.array([0.0,1.0,0.0]))
@@ -41,28 +55,30 @@ class TestMole(unittest.TestCase):
         self.assertEqual(self.mole[1].name,'O1')
         # @property mymolecule
         self.assertEqual(str(self.mole.atom(1).mymolecule.name),'H2O2')
-    def test_bond(self):
-        # Addbond
-        self.mole.addbond(1,2)
-        self.mole.addbond(1,3)
-        self.mole.addbond(3,4)
-        # isinstance
-        self.assertTrue(isinstance(self.mole.bond(1,3),rxmol.Bond))
+    # test_bond(self):
         # Bondlength
         self.assertEqual(self.mole.bond(1,3).length,0.5291772086)
-
-    def test_angle(self):
-        self.mole.addangle(2,1,3)
-        self.mole.addangle(4,3,1)
-        self.assertTrue(isinstance(self.mole.angle(2,1,3),rxmol.Angle))
+    # test_angle(self):
         self.assertEqual(self.mole.angle(2,1,3).anglevalue,90.0)
-    def test_dihd(self):
-        self.mole.adddihd(2,1,3,4)
-        self.assertTrue(isinstance(self.mole.dihd(4,3,1,2),rxmol.Dihd))
+    # test dihd
         self.assertEqual(self.mole.dihd(4,3,1,2).dihdvalue,90.0)
+    # test_bondfunc(self):
+        self.mole[1].atomtype='oh'
+        self.mole[2].atomtype='ho'
+        self.mole[3].atomtype='oh'
+        self.mole[4].atomtype='ho'
+        self.mole.addbondfunc(self.mole.bond(1,2))
+        self.mole.addbondfunc(self.mole.bond(1,3))
+        self.mole.addbondfunc(self.mole.bond(3,4))
+        self.mole.addanglefunc(self.mole.angle(2,1,3))
+        self.mole.addanglefunc(self.mole.angle(4,3,1))
+        self.mole.adddihdfunc(self.mole.dihd(2,1,3,4))
 
-
-
+    def test_readfile(self):
+        benz=rxmol.molecule("Benzene")
+        benz.readfromxyz("ben.xyz")
+        for atom in benz:
+            print(atom.name)
 if __name__=='__main__':
     import numpy as np
     unittest.main()
